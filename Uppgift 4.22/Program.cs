55fafb28e048b85c8ccdb84495573ba19c5e091b
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-20 13:21:32 +0200

    Add project files.

---
+++ Uppgift 4.22/Program.cs
@@ -0,0 +1,12 @@
+﻿for (int tal = 10;tal < 31; tal++)
+{
+    Console.WriteLine(tal);
+}
+for (int tal1 = 200; tal1 > 179; tal1--)
+{
+    Console.WriteLine(tal1);
+}
+for (int tal2 = 1000; tal2 <= 1400; tal2 += 50)
+{
+    Console.WriteLine(tal2);
+}
